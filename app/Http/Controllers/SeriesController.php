d880df2e89671d661a427fc5e9e470f72d9098cc
Lucas Coelho Santos <lucascoelho375@gmail.com>
2023-06-23 08:52:11 -0300

    add: adiciona todos os arquivos utilizados durante o curso

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Series;
+use App\Repositories\SeriesRepository;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function __construct(private SeriesRepository $repository)
+    {
+        $this->middleware('auth')->except('index');
+    }
+
+    public function index(Request $request)
+    {
+        $series = Series::all();
+        $mensagemSucesso = $request->session()->get('mensagem.sucesso');
+
+        return view('series.index')
+            ->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        $serie = $this->repository->add($request);
+
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+    }
+
+    public function edit(Series $series)
+    {
+        return view('series.edit')->with('series', $series);
+    }
+
+    public function update(SeriesFormRequest $request, Series $series)
+    {
+        $series->fill($request->all());
+        $series->save();
+        
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' atualizada com sucesso");
+    }
+
+    public function destroy(Series $series)
+    {
+        $series->delete();
+        //Series::destroy($request->series);
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', 'Série removida com sucesso');
+    }
+}
